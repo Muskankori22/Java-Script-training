3a33194394e86a6e23e4c5302e604abf4d67f1a5
Muskan Kori <muskankori024@gmail.com>
2024-11-23 15:23:20 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let score = 80
+let a = 75;
+if(score >= 80 && a>=75)
+{
+  console.log("Grade : 'A'");
+}
+else if(score >= 75 && a>=60)
+{
+  console.log("Grade : 'B'");
+}
+else if(score < 75 && a<=50)
+{
+  console.log("Grade : 'c'");
+}
+else{
+  console.log("Fail");
+}
